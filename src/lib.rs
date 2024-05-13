c6d81eb59168a4a71a6aca70f0012153c1496f5a
Thomas Sieverding <Thomas@bajix.com>
2024-05-12 23:07:10 -0700

    Use crate level cfg

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,8 @@
+#![cfg(any(target_arch = "wasm32", doc))]
 #![cfg_attr(feature = "strict_provenance", feature(strict_provenance))]
 
-#[cfg(any(target_arch = "wasm32", doc))]
 mod arena;
-#[cfg(any(target_arch = "wasm32", doc))]
 mod handle;
-#[cfg(any(target_arch = "wasm32", doc))]
 mod runtime;
 
-#[cfg(any(target_arch = "wasm32", doc))]
 pub use runtime::spawn_local;
